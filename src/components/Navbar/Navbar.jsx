c7a439c8dc47390c8093da5f1bb7b552b9fd60f0
TheJhonni <davideconi91@gmail.com>
2022-02-22 10:12:17 +0100

    logging out with onClick from firebase

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,27 +3,184 @@ import DropdownRender from "./Dropdown";
 import ProfileImg from "./ProfileImg";
 import { IoMdArrowDropdown } from "react-icons/io";
 import Logout from "./Logout";
+import { Transition } from "@headlessui/react";
 
 export default function Navbar() {
-  const [navbarOpen, setNavbarOpen] = useState(false);
-
-  const [isToggled, setIsToggled] = useState(false);
-
+  const [isOpen, setIsOpen] = useState(false);
   const [sureToLogout, setSureToLogout] = useState(false);
-
-  function toggleLogout() {
-    setIsToggled(!isToggled);
-  }
-
   return (
-    <>
-      <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 w-screen shadow-md mb-5">
+    <div>
+      <nav className="bg-gray-800">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex items-center justify-between h-16">
+            <div className="flex items-center">
+              <div className="flex-shrink-0">
+                <ProfileImg
+                  img={
+                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiqkuHvwY9wnqd1tV53y2GZYdSHNZ7q-JjTA&usqp=CAU"
+                  }
+                />
+              </div>
+              <div className="hidden md:block">
+                <div className="ml-10 flex items-baseline justify-between space-x-4">
+                  <a
+                    href="#"
+                    className=" hover:bg-gray-700 text-white px-3 py-2 rounded-md text-sm font-medium"
+                  >
+                    Dashboard
+                  </a>
+
+                  <a
+                    href="#"
+                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  >
+                    Team
+                  </a>
+
+                  <a
+                    href="#"
+                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  >
+                    Projects
+                  </a>
+
+                  <a
+                    href="#"
+                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  >
+                    Calendar
+                  </a>
+
+                  <a
+                    href="#"
+                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  >
+                    Reports
+                  </a>
+                  <div className="ml-auto">
+                    <span
+                      onClick={() => setSureToLogout(!sureToLogout)}
+                      className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                    >
+                      Logout
+                    </span>
+                  </div>
+                  {sureToLogout && <Logout />}
+                </div>
+              </div>
+            </div>
+            <div className="-mr-2 flex md:hidden">
+              <button
+                onClick={() => setIsOpen(!isOpen)}
+                type="button"
+                className="bg-gray-900 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                aria-controls="mobile-menu"
+                aria-expanded="false"
+              >
+                <span className="sr-only">Open main menu</span>
+                {!isOpen ? (
+                  <svg
+                    className="block h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    className="block h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                )}
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <Transition
+          show={isOpen}
+          enter="transition ease-out duration-100 transform"
+          enterFrom="opacity-0 scale-95"
+          enterTo="opacity-100 scale-100"
+          leave="transition ease-in duration-75 transform"
+          leaveFrom="opacity-100 scale-100"
+          leaveTo="opacity-0 scale-95"
+        >
+          {(ref) => (
+            <div className="md:hidden" id="mobile-menu">
+              <div ref={ref} className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+                <a
+                  href="#"
+                  className="hover:bg-gray-700 text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  Dashboard
+                </a>
+
+                <a
+                  href="#"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  Team
+                </a>
+
+                <a
+                  href="#"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  Projects
+                </a>
+
+                <a
+                  href="#"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  Calendar
+                </a>
+
+                <a
+                  href="#"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  Reports
+                </a>
+                <a
+                  href="#"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  Logout
+                </a>
+              </div>
+            </div>
+          )}
+        </Transition>
+      </nav>
+    </div>
+  );
+}
+
+{
+  /* <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 w-screen shadow-md mb-5">
         <div className="container mx-auto flex flex-wrap items-center justify-between">
-          <ProfileImg
-            img={
-              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiqkuHvwY9wnqd1tV53y2GZYdSHNZ7q-JjTA&usqp=CAU"
-            }
-          />
+         
           <DropdownRender />
           <button
             className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
@@ -49,37 +206,26 @@ export default function Navbar() {
                   <i className="fab fa-twitter text-lg leading-lg text-white opacity-75"></i>
                   <span className="ml-2">Tweet</span>
                 </a>
-              </li> */}
-              <li className="nav-item mr-2">
-                <ProfileImg
-                  img={
-                    "https://media-exp1.licdn.com/dms/image/C4E03AQEgNFJ49Vnc1w/profile-displayphoto-shrink_400_400/0/1642584038544?e=1649894400&v=beta&t=rp56U7xamh1EJue1VcESjTbiPVHHhgFV1RF9eVIuLjY"
-                  }
-                />
-              </li>
-            </ul>
-
-            <div className="ml-2">
-              <IoMdArrowDropdown
-                onClick={toggleLogout}
-                className="text-white text-2xl cursor-pointer"
-              />
-              {/* Logout onClick hidden div => display toggled */}
-              {isToggled && (
-                <div className="mt-2 ml-auto bg-white ">
-                  <span
-                    onClick={() => setSureToLogout(!sureToLogout)}
-                    className="text-blue p-5 cursor-pointer hover:text-[#05051df1]"
-                  >
-                    Logout
-                  </span>
-                </div>
-              )}
-              {sureToLogout && <Logout />}
-            </div>
-          </div>
-        </div>
-      </nav>
-    </>
-  );
+              </li> */
 }
+//         <li className="nav-item mr-2">
+//           <ProfileImg
+//             img={
+//               "https://media-exp1.licdn.com/dms/image/C4E03AQEgNFJ49Vnc1w/profile-displayphoto-shrink_400_400/0/1642584038544?e=1649894400&v=beta&t=rp56U7xamh1EJue1VcESjTbiPVHHhgFV1RF9eVIuLjY"
+//             }
+//           />
+//         </li>
+//       </ul>
+
+//       <div className="ml-2">
+//         <IoMdArrowDropdown
+//           onClick={toggleLogout}
+//           className="text-white text-2xl cursor-pointer"
+//         />
+//         {/* Logout onClick hidden div => display toggled */}
+//         {isToggled && (
+//
+//       </div>
+//     </div>
+//   </div>
+// </nav>
--- src/screens/PostScreen.jsx
+++ src/screens/PostScreen.jsx
@@ -10,78 +10,60 @@ export default function Post({ post }) {
   }, []);
 
   return (
-    <div className="img-textLeft">
-      <Navbar />
-
-      <main className="relative container mx-auto bg-white px-4">
-        <div className="relative -mx-4 top-0 pt-[17%] overflow-hidden">
-          <img
-            className="absolute inset-0 object-cover object-top w-full h-full filter blur"
-            src="https://images.unsplash.com/photo-1514525253161-7a46d19cd819?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8Y29uY2VydCUyMHBvc3RlcnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1200&q=80"
-            alt=""
-          />
-        </div>
-
-        <div className="mt-[-10%] w-1/2 mx-auto">
-          <div className="relative pt-[56.25%] overflow-hidden rounded-2xl">
+    <>
+      <div className="img-textLeft">
+        <Navbar />
+        <main className="relative z-100 container mx-auto bg-[#ffffffd2] px-4">
+          <div className="relative -mx-4 top-0 pt-[17%] overflow-hidden">
             <img
-              className="w-full h-full absolute inset-0 object-cover"
-              src={post[0].img1}
+              className="absolute inset-0 object-cover object-top w-full h-full filter blur"
+              src="https://images.unsplash.com/photo-1514525253161-7a46d19cd819?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8Y29uY2VydCUyMHBvc3RlcnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1200&q=80"
               alt=""
             />
           </div>
-        </div>
 
-        <article className="max-w-prose mx-auto py-8">
-          <h1 className="text-2xl font-bold">{post[0].title}</h1>
-          <h2 className="mt-2 text-sm text-gray-500">
-            <a href={post[0].authorLink}> {post[0].author}</a>, 28 November 2021
-          </h2>
-          <h2 className="mt-2 text-sm text-gray-500">
-            {post[0].p}, <br />{" "}
-            <a href={post[0].link}>CLICK HERE to read the Original Article</a>
-          </h2>
+          <div className="mt-[-10%] w-1/2 mx-auto">
+            <div className="relative pt-[56.25%] overflow-hidden rounded-2xl">
+              <img
+                className="w-full h-full absolute inset-0 object-cover"
+                src={post[0].img1}
+                alt=""
+              />
+            </div>
+          </div>
 
-          <p className="mt-6">{post[0].body}</p>
-          {/* <p className="mt-4">
-            Praesent ornare interdum gravida. Donec efficitur leo suscipit
-            aliquet pellentesque. In quis purus et dui mollis vulputate. Aenean
-            non faucibus felis. Phasellus non aliquet est, non dictum sem. In
-            hac habitasse platea dictumst. Integer vehicula elit ac libero
-            egestas ornare non sed dolor. Integer vulputate id est nec pulvinar.
-            Cras nec sollicitudin lacus, quis sagittis diam. Donec porta libero
-            ac lorem semper, eget porttitor quam fermentum. Ut tincidunt feugiat
-            sem, nec aliquam mi tincidunt non.
-          </p>
-          <p className="mt-4">
-            Etiam accumsan leo sem, sit amet faucibus ex convallis fermentum.
-            Nunc tristique, eros eget rutrum accumsan, dolor quam varius nisl,
-            ut euismod arcu urna a lectus. Mauris at dapibus metus. Vestibulum
-            ipsum lorem, dictum vitae sapien eget, rutrum rhoncus sapien.
-            Vivamus a nisi ut risus porta ultricies. Etiam mollis massa odio,
-            non eleifend leo ullamcorper in. Sed ultricies, magna id fermentum
-            volutpat, lorem orci placerat mauris, et molestie ipsum mauris sed
-            sapien. Aliquam nulla lorem, pretium ut interdum dapibus, suscipit
-            at metus. Proin consequat euismod consequat. Aenean placerat turpis
-            et pretium condimentum. Nunc hendrerit tellus semper suscipit
-            dignissim. Nullam fringilla, sem nec volutpat tincidunt, ex eros
-            congue diam, quis venenatis mauris urna et dolor. Vivamus aliquam
-            euismod eros vel pulvinar. Cras arcu augue, rutrum nec velit sit
-            amet, aliquet lobortis leo. Donec placerat, libero in blandit
-            mattis, turpis nisl varius urna, ac pellentesque dui nisi sit amet
-            dui.
-          </p>
-          <p className="mt-4">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel
-            sapien nisi. Sed molestie quis odio ut facilisis. Duis porttitor
-            turpis at sollicitudin ornare. Aenean vitae nulla justo. In egestas
-            consequat sem, dapibus gravida velit. Nullam dictum, ligula vel
-            porttitor vestibulum, est enim lobortis erat, eget rutrum nunc ex at
-            mauris. Nunc nec laoreet ipsum. Sed vel est a eros sagittis
-            vulputate. Proin cursus ut mauris vel commodo. Sed non sapien metus.
-          </p> */}
-        </article>
-      </main>
-    </div>
+          <article className="max-w-prose mx-auto py-8">
+            <h1 className="text-2xl font-bold">{post[0].title}</h1>
+            <h2 className="mt-2 text-sm text-gray-500">
+              <a className="hover:text-blue-700" href={post[0].authorLink}>
+                {post[0].author}
+              </a>
+              , 28 November 2021
+            </h2>
+            <h2 className="mt-2 text-sm text-gray-500">
+              {post[0].p}, <br />{" "}
+              <a
+                className="hover:text-blue-700 text-xs mt-3"
+                href={post[0].link}
+              >
+                CLICK HERE to read the Original Article
+              </a>
+            </h2>
+
+            <p className="mt-6">{post[0].body[0]}</p>
+            <p className="mt-4">{post[0].body[1]}</p>
+            <p className="mt-4">{post[0].body[2]}</p>
+            <p className="mt-4">{post[0].body[3]}</p>
+            <p className="mt-4">{post[0].body[4]}</p>
+            <p className="mt-4">{post[0].body[5]}</p>
+            <p className="mt-4">{post[0].body[6]}</p>
+            <p className="mt-4">{post[0].body[7]}</p>
+            <p className="mt-4">{post[0].body[8]}</p>
+            <p className="mt-4">{post[0].body[9]}</p>
+            <p className="mt-4">{post[0].body[10]}</p>
+          </article>
+        </main>
+      </div>
+    </>
   );
 }
