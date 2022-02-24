6aaed9782d105b389a283e0321d06c0167e99a69
TheJhonni <davideconi91@gmail.com>
2022-02-24 18:49:57 +0100

    acqua css spinner done

--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -5,15 +5,34 @@ function Spinner() {
   return (
     <div id="cont">
       <section id="section">
-        <div className="loader">
-          <span id="span1"></span>
-          <span id="span2"></span>
-          <span id="span3"></span>
-          <span id="span4"></span>
-          <span id="span5"></span>
-          <span id="span6"></span>
-          <span id="span7"></span>
-          <span id="span8"></span>
+        <svg id="arturo">
+          <filter id="gooey">
+            <feGaussinianBlur in="SourceGraphic" stdDeviation="10" />
+            <feColorMatrix
+              values="
+                      1 0 0 0 0
+                      0 1 0 0 0
+                      0 0 1 0 0
+                      0 0 0 20 -10
+                                          
+                      "
+            ></feColorMatrix>
+          </filter>
+        </svg>
+        <div id="loader">
+          <span style={{ "--i": 1 }}></span>
+          <span style={{ "--i": 2 }}></span>
+          <span style={{ "--i": 3 }}></span>
+          <span style={{ "--i": 4 }}></span>
+          <span style={{ "--i": 5 }}></span>
+          <span style={{ "--i": 6 }}></span>
+          <span style={{ "--i": 7 }}></span>
+          <span style={{ "--i": 8 }}></span>
+          <span className="rotate" style={{ "--j": 0 }}></span>
+          <span className="rotate" style={{ "--j": 1 }}></span>
+          <span className="rotate" style={{ "--j": 2 }}></span>
+          <span className="rotate" style={{ "--j": 3 }}></span>
+          <span className="rotate" style={{ "--j": 4 }}></span>
         </div>
       </section>
     </div>
