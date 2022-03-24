0c368ac258f1fb1f4186453921edd6a569fd1764
TheJhonni <davideconi91@gmail.com>
2022-03-24 14:25:02 +0100

    changing role users, deleting them

---
+++ src/components/components_2nd_Layer/users/ChangeRole.jsx
@@ -0,0 +1,108 @@
+import React, { useState } from "react";
+import firebase from "firebase/compat/app";
+import { RiAdminFill } from "react-icons/ri";
+import { doc, updateDoc } from "firebase/firestore";
+
+export default function ChangeRole({ frero, role }) {
+  const [newRole, setNewRole] = useState("");
+  const [showModal, setShowModal] = useState(false);
+
+  const handleChange = (e) => {
+    console.log(e.target.value);
+    setNewRole(e.target.value);
+  };
+
+  const updateRole = () => {
+    setTimeout(() => {
+      firebase
+        .firestore()
+        .collection("users")
+        .doc(frero.id)
+        .update({ role: newRole })
+        .then(() => {
+          alert("role changed!");
+        });
+    }, 200);
+    setShowModal(false);
+  };
+
+  return (
+    <>
+      <button
+        className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+        type="button"
+        onClick={() => setShowModal(true)}
+      >
+        <RiAdminFill />
+      </button>
+      {showModal ? (
+        <>
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start mx-auto justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
+                  <h3 className="text-3xl font-semibold">Chane Role</h3>
+                  <button
+                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onClick={() => setShowModal(false)}
+                  >
+                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                      ×
+                    </span>
+                  </button>
+                </div>
+
+                <div className="relative p-6 flex justify-center space-x-5 items-center">
+                  <div className="flex justify-center items-center">
+                    <input
+                      className="mx-1"
+                      type="radio"
+                      id="admin"
+                      name="drone"
+                      value="Admin"
+                      onClick={handleChange}
+                      defaultChecked={role === "Admin"}
+                    />
+                    <label for="admin">Admin</label>
+                  </div>
+                  <div className="flex justify-center items-center">
+                    <input
+                      className="mx-1"
+                      type="radio"
+                      id="Client/Guest"
+                      name="drone"
+                      value="Client/Guest"
+                      onClick={handleChange}
+                      defaultChecked={role === "Client/Guest"}
+                    />
+                    <label for="Client/Guest">Client/Guest</label>
+                  </div>
+                </div>
+                {/*footer*/}
+                <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
+                  <button
+                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Close
+                  </button>
+                  <button
+                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={updateRole}
+                  >
+                    Save Changes
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
+    </>
+  );
+}
--- src/components/components_2nd_Layer/users/ListUsers.jsx
+++ src/components/components_2nd_Layer/users/ListUsers.jsx
@@ -1,36 +1,43 @@
 import firebase from "firebase/compat/app";
 import { useEffect, useState } from "react";
-import { db } from "../../../firebase";
+import ChangeRole from "./ChangeRole";
+import { FcFullTrash } from "react-icons/fc";
 
 export default function ListUsers() {
-  const [userName, setUserName] = useState("");
-  const [useremail, setUserEmail] = useState("");
-  const [userRole, setUserRole] = useState("");
+  const [userInfo, setUserInfo] = useState([]);
 
-  const [user, setUser] = useState([]);
+  const deleteUser = (id) => {
+    setTimeout(() => {
+      console.log(id);
+      firebase
+        .firestore()
+        .collection("users")
+        .doc(id)
+        .delete()
+        .then(() => {
+          alert("Deleted!");
+        });
+    }, 200);
+  };
 
   useEffect(() => {
     firebase
       .firestore()
       .collection("users")
-      .onSnapshot("value", (datasnapshot) => {
-        datasnapshot.docs.forEach((element) => {
-          console.log(element);
+      .onSnapshot((snapshot) => {
+        const userArray = snapshot.docs.map((doc) => {
+          return { id: doc.id, ...doc.data() };
         });
+        setUserInfo(userArray);
       });
-    // console.log("All data in 'users' collection", snapshot);
-    // const newArr = snapshot.docs.map((doc) => doc.data());
-    // console.log(newArr);
-    // setUser([{ id: newArr.id, ...newArr }]);
-    // console.log(user);
   }, []);
 
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col py-10 mt-5">
       <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
         <div className="py-4 inline-block min-w-full sm:px-6 lg:px-8">
           <div className="overflow-hidden">
-            <table className="min-w-full text-center border-b">
+            <table className="min-w-full text-center border-b rounded">
               <thead className="border-b bg-gray-800">
                 <tr>
                   <th
@@ -57,43 +64,49 @@ export default function ListUsers() {
                   >
                     Role
                   </th>
+                  <th
+                    scope="col"
+                    className="text-sm font-medium text-white px-6 py-4"
+                  >
+                    Change Role
+                  </th>
+                  <th
+                    scope="col"
+                    className="text-sm font-medium text-white px-6 py-4"
+                  >
+                    Delete User
+                  </th>
                 </tr>
               </thead>
               <tbody className="bg-white">
-                <tr className="bg-white border-b">
-                  <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                    ciuabcigbua
-                  </td>
-                  <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                    ciuabcigbua
-                  </td>
-                  <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                    ciuabcigbua
-                  </td>
-                  <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                    ciuabcigbua
-                  </td>
-                </tr>
-
-                {/* {userInfo &&
-                  userInfo.map((frero) => {
-                    <tr className="bg-white border-b">
+                {userInfo &&
+                  userInfo.map((frero, i) => (
+                    <tr key={i} className="bg-white border-b">
                       <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                        1
+                        {i + 1}
                       </td>
                       <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                         {frero.displayName}
-                        euvscin
                       </td>
                       <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                         {frero.email}
                       </td>
                       <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                        {eachUser.role}
-                        euvscin
+                        {frero.role}
                       </td>
-                    </tr>;
-                  })} */}
+                      <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                        <ChangeRole frero={frero} role={frero.role} />
+                      </td>
+                      <td className="font-light flex items-center justify-center px-6 py-4 whitespace-nowrap">
+                        <FcFullTrash
+                          onClick={() => {
+                            deleteUser(frero.id);
+                          }}
+                          className="text-3xl cursor-pointer hover:scale-125 transition duration-75 ease-in"
+                        />
+                      </td>
+                    </tr>
+                  ))}
               </tbody>
             </table>
           </div>
